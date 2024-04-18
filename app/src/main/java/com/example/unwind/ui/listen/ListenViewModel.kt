f707facb66113ea819870cc89aa2e1a9df266472
Nyx <lilizhao107513@gmail.com>
2024-04-18 11:52:14 -0400

    search button, changing up listen frag

--- app/src/main/java/com/example/unwind/ui/listen/ListenViewModel.kt
+++ app/src/main/java/com/example/unwind/ui/listen/ListenViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ListenViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is listen Fragment"
     }
     val text: LiveData<String> = _text
 }
