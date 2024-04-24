c60a994369948e933ac994a8b84884923959faae
Nyx <lilizhao107513@gmail.com>
2024-04-24 16:31:53 -0400

    premum page in the works

--- app/src/main/java/com/example/unwind/api/QuotesApiService.kt
+++ app/src/main/java/com/example/unwind/api/QuotesApiService.kt
@@ -3,6 +3,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 import com.example.unwind.model.Quote
 interface QuotesApiService {
-    @GET("/random")
+    @GET("/random?maxLength=80")
     fun getRandomQuote(): Call<Quote>
 }
--- app/src/main/java/com/example/unwind/ui/SettingsActivity.kt
+++ app/src/main/java/com/example/unwind/ui/SettingsActivity.kt
@@ -1,39 +1,15 @@
-//package com.example.unwind
-//
-//import android.os.Bundle
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.preference.PreferenceFragmentCompat
-//
-//class SettingsActivity : AppCompatActivity() {
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.settings_activity)
-//        if (savedInstanceState == null) {
-//            supportFragmentManager
-//                .beginTransaction()
-//                .replace(R.id.settings, SettingsFragment())
-//                .commit()
-//        }
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//    }
-//
-//    class SettingsFragment : PreferenceFragmentCompat() {
-//        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-//            setPreferencesFromResource(R.xml.root_preferences, rootKey)
-//        }
-//    }
-//}
 package com.example.unwind.ui
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.unwind.R
 
 class SettingsActivity : AppCompatActivity() {
-    private lateinit var premiumButton : Button
+    //private lateinit var premiumButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings)
@@ -45,14 +21,17 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val logoutButton = findViewById<View>(R.id.logout_button)
-        logoutButton.setOnClickListener {
+        val logout = findViewById<View>(R.id.logout)
+        logout.setOnClickListener {
+            Log.d("SettingsActivity", "Logoutbutton clicked")
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
-        val logout = findViewById<View>(R.id.logout)
-        logout.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
+
+        val premium = findViewById<TextView>(R.id.unwind_prem)
+        premium.setOnClickListener {
+            Log.d("SettingsActivity", "Premium button clicked")
+            val intent = Intent(this, PremiumActivity::class.java)
             startActivity(intent)
         }
         /*premiumButton.setOnClickListener{
